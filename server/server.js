dc96723a8cecd006808a113f2bd56b5088e6d1fe
Chris Phelps <c.j.phelps89@gmail.com>
2025-01-06 12:58:30 -0500

    Updated headers in server file for deployment

--- server/server.js
+++ server/server.js
@@ -31,10 +31,22 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
     app.use((req, res, next) => {
-      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      res.setHeader(
+        "Access-Control-Allow-Origin",
+        "https://blunders-birth.onrender.com/"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+      );
+      res.setHeader("Access-Control-Allow-Credentials", true);
+      res.setHeader("Access-Control-Allow-Private-Network", true);
+      //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+      res.setHeader("Access-Control-Max-Age", 7200);
       next();
     });
 
